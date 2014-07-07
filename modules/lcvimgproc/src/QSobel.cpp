6367ec83a32bca30729e48437df2aa415f7a74a8
Dinu SV <mail@dinusv.com>
2014-07-07 14:22:52 +0300

    Updated license.

--- modules/lcvimgproc/src/QSobel.cpp
+++ modules/lcvimgproc/src/QSobel.cpp
@@ -1,3 +1,18 @@
+/****************************************************************************
+**
+** Copyright (C) 2014 Dinu SV.
+** (contact: mail@dinusv.com)
+** This file is part of Live CV application.
+**
+** GNU General Public License Usage
+** 
+** This file may be used under the terms of the GNU General Public License 
+** version 3.0 as published by the Free Software Foundation and appearing 
+** in the file LICENSE.GPL included in the packaging of this file.  Please 
+** review the following information to ensure the GNU General Public License 
+** version 3.0 requirements will be met: http://www.gnu.org/copyleft/gpl.html.
+**
+****************************************************************************/
 #include "QSobel.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "QMatNode.hpp"
