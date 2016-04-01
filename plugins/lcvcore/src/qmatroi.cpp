c1cb001ba206aa9efb3d98c342e5b05c7ace47a0
Dinu SV <mail@dinusv.com>
2016-04-01 09:54:53 +0300

    Fixed mat roi bug when selecting full image.

--- plugins/lcvcore/src/qmatroi.cpp
+++ plugins/lcvcore/src/qmatroi.cpp
@@ -101,6 +101,6 @@ QMatRoi::QMatRoi(QQuickItem *parent)
   \a out
  */
 void QMatRoi::transform(cv::Mat &in, cv::Mat &out){
-    if ( in.cols > m_regionX + m_regionWidth && in.rows > m_regionY + m_regionHeight )
+    if ( in.cols >= m_regionX + m_regionWidth && in.rows >= m_regionY + m_regionHeight )
         in(cv::Rect(m_regionX, m_regionY, m_regionWidth, m_regionHeight)).copyTo(out);
 }
