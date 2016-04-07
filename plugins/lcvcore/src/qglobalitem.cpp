bc76a4bc87aaca72fa2ce9faa2dd6011ec8a7da5
Dinu SV <mail@dinusv.com>
2016-04-07 19:35:57 +0300

    Fixed item reload positioning. Fixed sample.

--- plugins/lcvcore/src/qglobalitem.cpp
+++ plugins/lcvcore/src/qglobalitem.cpp
@@ -2,20 +2,11 @@
 #include "qstatecontainer.h"
 #include <QQmlEngine>
 
-void debugChildren(QList<QQuickItem*> children){
-    for ( int i = 0; i < children.size(); ++i ){
-        qDebug() << children[i];
-    }
-}
-
-
 class QGlobalItemState{
 public:
     QGlobalItemState() : item(0){}
     QQuickItem* item;
 };
-
-QGlobalItemState* st = new QGlobalItemState;
 
 QGlobalItem::QGlobalItem(QQuickItem* parent)
     : QQuickItem(parent)
@@ -26,31 +17,27 @@ QGlobalItem::QGlobalItem(QQuickItem* parent)
 }
 
 QGlobalItem::~QGlobalItem(){
-//    if ( m_state ){
-//        m_state->item->setParentItem(0);
-//    }
-//    if ( m_stateId == "" )
-//        delete m_state;
+    if ( m_stateId == "" )
+        delete m_state;
 }
 
 void QGlobalItem::setStateId(const QString& arg){
     if (m_stateId == arg)
         return;
-//    if (m_stateId == "")
-//        delete m_state;
-//    else
-//        m_state = 0;
+    if (m_stateId == "")
+        delete m_state;
+    else
+        m_state = 0;
 
     m_stateId = arg;
     emit stateIdChanged(arg);
 
-//    sync();
+    sync();
 }
 
 void QGlobalItem::sync(){
     if ( !isComponentComplete() || m_source == 0 )
         return;
-    qDebug() << "--Sync";
 
     if ( !m_state ){
         if ( m_stateId != "" ){
@@ -61,21 +48,8 @@ void QGlobalItem::sync(){
                 reload();
                 stateContainer.registerState(m_stateId, m_state);
             } else {
-                qDebug() << "--Sync without reload";
-//                m_state = new QGlobalItemState;
-//                QQuickItem* item = qobject_cast<QQuickItem*>(m_source->create(m_source->creationContext()));
-
-//                m_state->item = item;
                 m_state->item->setParentItem(parentItem());
                 m_state->item->stackAfter(this);
-
-//                m_state->item->polish();
-//                qmlEngine(this)->setObjectOwnership(m_state->item, QQmlEngine::CppOwnership);
-//                emit itemChanged();
-//                debugChildren(parentItem()->childItems());
-
-                //HERE
-//                qDebug() << "PARENT ITEM: " << parentItem();
             }
         } else {
             qCritical("GlobalItem will not function without a stateId assigned. "
@@ -100,7 +74,6 @@ QQuickItem* QGlobalItem::item() const{
 void QGlobalItem::reload(){
     if ( !isComponentComplete() || m_source == 0 )
         return;
-    qDebug() << "--Reload";
 
     QQuickItem* item = qobject_cast<QQuickItem*>(m_source->create(m_source->creationContext()));
     if ( item == 0 ){
--- samples/core/globalitem.qml
+++ samples/core/globalitem.qml
@@ -3,32 +3,26 @@ import lcvcore 1.0
 Column{
 
     GlobalItem{
+        id : globalItem
         stateId : "state1"
-    
+     
         source : Rectangle{
             width : 100
             height : 100
             color : "#ff0000"
-            MouseArea{
-                anchors.fill : parent
-                onClicked : {
-                    parent.color = "#ff00ff"
-                    parent.height = 100
-                }
-            }
-        }
+        }   
     }
     
     Rectangle{
-            width : 100
-            height : 100
-            color : "#fff000"
-            MouseArea{
-                anchors.fill : parent
-                onClicked : {
-                    parent.color = "#ff00ff"
-                    parent.height = 100
-                }
+        width : 100
+        height : 100
+        color : "#fff000"
+        MouseArea{
+            anchors.fill : parent
+            onClicked : {
+                globalItem.item.color = "#ff00ff"
+                parent.color = "#ff00ff"
             }
         }
+    }
 }
