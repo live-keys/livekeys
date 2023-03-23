7a6eedbb2d5048fa2f8a9e2b69a6b53997c3ac27
Dinu SV <mail@dinusv.com>
2023-03-23 20:59:12 +0200

    elements.LanguageNodes: Js Conversion updates and fixes.

--- lib/lvelements/src/compiler/languagenodes_p.h
+++ lib/lvelements/src/compiler/languagenodes_p.h
@@ -190,6 +190,13 @@ public:
     ObjectNode(const TSNode& node, const std::string& typeString = "Object") : BaseNode(node, typeString){}
 };
 
+
+class ExpressionNode: public BaseNode {
+    friend class BaseNode;
+public:
+    ExpressionNode(const TSNode& node, const std::string& typeString = "Expression") : BaseNode(node, typeString){}
+};
+
 class ReturnStatementNode: public BaseNode {
     friend class BaseNode;
 public:
@@ -432,6 +439,69 @@ private:
 
 };
 
+class TypedMethodDeclarationNode : public BaseNode{
+    friend class BaseNode;
+public:
+    TypedMethodDeclarationNode(const TSNode& node);
+    virtual std::string toString(int indent = 0) const;
+
+    IdentifierNode* name() const{ return m_name; }
+    ParameterListNode* parameters() const{ return m_parameters; }
+    JsBlockNode* body() const{ return m_body; }
+
+    void setAsync(bool async){ m_async = async; }
+    void setStatic(bool value){ m_static = value; }
+
+    bool isAsync() const{ return m_async; }
+    bool isStatic() const{ return m_static; }
+
+protected:
+    TypedMethodDeclarationNode(const TSNode& node, const std::string& typeString);
+
+private:
+    IdentifierNode*    m_name;
+    ParameterListNode* m_parameters;
+    JsBlockNode*       m_body;
+    bool               m_async;
+    bool               m_static;
+};
+
+class PropertyAccessorDeclarationNode : public TypedMethodDeclarationNode{
+
+public:
+    friend class BaseNode;
+
+    enum Access{
+        Unknown = 0,
+        Getter,
+        Setter
+    };
+
+    class PropertyAccess{
+    public:
+        PropertyAccess() : getter(nullptr), setter(nullptr){}
+
+        PropertyAccessorDeclarationNode* getter;
+        PropertyAccessorDeclarationNode* setter;
+    };
+
+public:
+    PropertyAccessorDeclarationNode(const TSNode& node)
+        : TypedMethodDeclarationNode(node, "PropertyAccessorDeclaration")
+        , m_access(PropertyAccessorDeclarationNode::Unknown)
+        , m_isPropertyAttached(false)
+    {}
+
+    Access access() const{ return m_access; }
+    void setIsPropertyAttached(bool attached) { m_isPropertyAttached = attached; }
+    bool isPropertyAttached() const{ return m_isPropertyAttached; }
+    IdentifierNode* firstParameterName() const;
+
+private:
+    Access m_access;
+    bool   m_isPropertyAttached;
+};
+
 class PropertyDeclarationNode : public JsBlockNode{
 
     friend class BaseNode;
@@ -452,6 +522,15 @@ public:
 
     bool hasAssignment(){ return m_expression != nullptr || m_statementBlock != nullptr; }
     bool isBindingsAssignment(){ return m_isBindingAssignment; }
+
+    void convertToJs(
+        const std::string& source,
+        const std::string& componentRef,
+        int indt,
+        ConversionContext* ctx,
+        const PropertyAccessorDeclarationNode::PropertyAccess& propertyAccess,
+        ElementsInsertion *compose
+    );
 
 private:
     IdentifierNode* m_name;
@@ -535,7 +614,6 @@ public:
 private:
     ConstructorDefinitionNode* m_constructor;
     friend class ComponentDeclarationNode;
-
 };
 
 class ComponentDeclarationNode : public JsBlockNode{
@@ -553,6 +631,8 @@ public:
 
     void pushToAssignments(PropertyAssignmentNode* pan) { m_assignments.push_back(pan); }
     std::vector<PropertyAssignmentNode*>& assignments() { return m_assignments; }
+
+    PropertyAccessorDeclarationNode::PropertyAccess propertyAccessors(const std::string& source, const std::string& propertyName);
 
     std::string name(const std::string &source) const;
     const std::vector<IdentifierNode*> heritage() const{ return m_heritage; }
@@ -727,10 +807,12 @@ public:
     IdentifierNode* name() const{ return m_name; }
     ParameterListNode* parameterList() const{ return m_parameters; }
     JsBlockNode* body() const{ return m_body; }
+    ExpressionNode* bodyExpression() const{ return m_bodyExpression; }
 private:
     IdentifierNode*    m_name;
     ParameterListNode* m_parameters;
     JsBlockNode*       m_body;
+    ExpressionNode*    m_bodyExpression;
 };
 
 class MethodDefinitionNode : public BaseNode{
@@ -794,61 +876,6 @@ private:
     IdentifierNode* m_name;
 };
 
-
-class TypedMethodDeclarationNode : public BaseNode{
-    friend class BaseNode;
-public:
-    TypedMethodDeclarationNode(const TSNode& node);
-    virtual std::string toString(int indent = 0) const;
-
-    IdentifierNode* name() const{ return m_name; }
-    ParameterListNode* parameters() const{ return m_parameters; }
-    JsBlockNode* body() const{ return m_body; }
-
-    void setAsync(bool async){ m_async = async; }
-    void setStatic(bool value){ m_static = value; }
-
-    bool isAsync() const{ return m_async; }
-    bool isStatic() const{ return m_static; }
-
-protected:
-    TypedMethodDeclarationNode(const TSNode& node, const std::string& typeString);
-
-private:
-    IdentifierNode*    m_name;
-    ParameterListNode* m_parameters;
-    JsBlockNode*       m_body;
-    bool               m_async;
-    bool               m_static;
-};
-
-class PropertyAccessorDeclarationNode : public TypedMethodDeclarationNode{
-
-public:
-    friend class BaseNode;
-
-    enum Access{
-        Unknown = 0,
-        Getter,
-        Setter
-    };
-
-public:
-    PropertyAccessorDeclarationNode(const TSNode& node)
-        : TypedMethodDeclarationNode(node, "PropertyAccessorDeclaration")
-        , m_access(PropertyAccessorDeclarationNode::Unknown)
-        , m_isPropertyAttached(false)
-    {}
-
-    Access access() const{ return m_access; }
-    void setIsPropertyAttached(bool attached) { m_isPropertyAttached = attached; }
-    bool isPropertyAttached() const{ return m_isPropertyAttached; }
-    IdentifierNode* firstParameterName() const;
-
-private:
-    Access m_access;
-    bool   m_isPropertyAttached;
-};
 
 }} // namespace lv, el
 
